c9a28510ac73aa3dec97dda3077c82d0845cb89b
mouh2020 <badimohammed2019@gmail.com>
2023-05-03 23:23:16 +0100

    correct stepsize

--- filters.py
+++ filters.py
@@ -4,7 +4,7 @@ from binance.client import Client
 client = Client()
 
 def quantity (symbol,balance,price) :
-    return round_step_size(float(balance)/float(price), client.get_symbol_info(symbol)["filters"][2]["stepSize"])
+    return round_step_size(float(balance)/float(price), client.get_symbol_info(symbol)["filters"][1]["stepSize"])
     
 def price (symbol,price) :
     return round_step_size(float(price), client.get_symbol_info(symbol)["filters"][0]["tickSize"])
